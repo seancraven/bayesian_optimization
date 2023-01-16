e4214cdb10938f270e5087fdd42a412e3ebcdf3e
seancraven <seanpietercraven@gmail.com>
2023-01-16 15:02:06 +0000

    formatting

--- gp.py
+++ gp.py
@@ -2,6 +2,7 @@
     Writing code for gp implemented in pytorch. 
 """
 import torch
+from torch import TensorType
 import gpytorch
 from gpytorch import kernels, models
 import matplotlib.pyplot as plt
@@ -19,7 +20,30 @@ class GaussianProcessSurrogate(models.ExactGP):
             kernels.MaternKernel(ard_num_dims=train_x.shape[1])
         )
 
-    def forward(self, x):
+    def forward(self, x: TensorType):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+
+def train_guassian_process(
+    g_p: GaussianProcessSurrogate,
+    train_x: TensorType,
+    train_y: TensorType,
+    iter: int,
+    verbose: bool = True,
+):
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = g_p(train_x, train_y, likelihood)
+
+    model.train()
+
+    opt = torch.optim.Adam(model.parameters())
+    mll = gpytorch.ExactMarginalLogLikelihood(likelihood, model)
+
+
+
+if __name__ == "__main__":
+    train_x = torch.linspace(-5, 5, 10)
+    train_y = torch.distributions.Normal(0, 5).sample(10)
+    test_y = torch.linspace(-5, 5, 50)
